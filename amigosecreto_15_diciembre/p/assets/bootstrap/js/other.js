6c4ceef8acd68e41047adf8863568aa05baaa273
Luis Morales <soy.lmml@gmail.com>
2021-12-03 17:01:06 -0600

    olga convivio 15 diciembre

---
+++ amigosecreto_15_diciembre/p/assets/bootstrap/js/other.js
@@ -0,0 +1,10 @@
+
+let myfriend = document.getElementById("hi");
+myfriend.innerHTML += " " + person;
+
+function hm() {
+    let myfriend = document.getElementById("friend");
+    let text = String.fromCharCode(...s_fr[s_f[person]]);
+    myfriend.innerHTML = text;
+}
+
